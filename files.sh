39ed401c75b09d2252208524c0df634d015a4b38
Caleb Powell <powellcalebm@gmail.com>
2021-09-21 08:38:17 -0400

    Quick Push

--- files.sh
+++ files.sh
@@ -1,10 +1,13 @@
-echo "File is located on /home/pi/Desktop/"
+echo "File is located on /home/pi/Desktop/CSC-241"
 read y
-cd /home/pi/Desktop/${y}
+cd /home/pi/Desktop/CSC-241/${y}
 echo "Enter Your file name: "
 read x
 gcc ${x}.c -S -o ${x}.s;
 as -a ${x}.s > ${x}.lst;
 gcc ${x}.c -o ${x};
 rm a.out;
+cd /home/pi/Desktop/CSC-241/
+zip -r ${x}.zip ${y};
+./github.sh;
 echo "Complete!"
