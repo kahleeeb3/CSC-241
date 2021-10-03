4da18e01eee579943901443e0048246adb76556f
Caleb Powell <powellcalebm@gmail.com>
2021-10-03 13:50:08 -0400

    New file script

--- files.sh
+++ files.sh
@@ -1,13 +1,14 @@
 echo "Enter folder name in /home/pi/Desktop/CSC-241/"
 read y
-cd /home/pi/Desktop/CSC-241/${y}
+cd /home/pi/Desktop/CSC-241/${y};
 #echo "Enter Your file name without extension: "
 #read x
 #gcc ${x}.c -S -o ${x}.s;
 #as -a ${x}.s > ${x}.lst;
 #gcc ${x}.c -o ${x};
 #rm a.out;
-cd /home/pi/Desktop/CSC-241/
+cd /home/pi/Desktop/CSC-241/;
+rm output.zip;
 zip -r output.zip ${y};
 ./github.sh;
 echo "Complete!"
